13c539797cf50589948da7729b38455cfc291af2
Shadow091101 <manavnaik442@gmail.com>
2026-06-09 17:32:53 +0530

    done some work on dashboard and also created the create post functionality for admin

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, request
+from flask import Flask,render_template, request, session,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail,Message
 import pymysql
@@ -20,6 +20,8 @@ app.config['MAIL_USE_TLS']=mail_params['mail_use_tls']
 app.config['MAIL_USERNAME']=mail_params['mail_username']
 app.config['MAIL_PASSWORD']=mail_params['mail_password']
 app.config['MAIL_DEFAULT_SENDER']=mail_params['mail_default_sender']
+
+app.secret_key='manavnaik442'
 
 mail=Mail(app)
 
@@ -44,7 +46,7 @@ class Contact(db.Model):
 class Posts(db.Model):
     srno = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), unique=False, nullable=False)
-    content = db.Column(db.String(150), unique=False, nullable=False)
+    content = db.Column(db.Text, nullable=False)
     author = db.Column(db.String (120), unique=False, nullable=False)
     slug=db.Column(db.String(100),unique=False,nullable=False)
     date = db.Column(db.DateTime,nullable=True)
@@ -52,7 +54,12 @@ class Posts(db.Model):
 
 @app.route("/")
 def index():
+    print("LOCAL_SERVER =", local_server)
+    print("DB URI =", app.config["SQLALCHEMY_DATABASE_URI"])
     posts = Posts.query.order_by(Posts.date.desc()).limit(3).all()
+    posts1 = Posts.query.all()
+    for p in posts1:
+        print(p.title, p.slug)
     return render_template("index.html", params=params, posts=posts,active="home")
 
 @app.route("/posts")
@@ -60,12 +67,89 @@ def all_posts():
     posts = Posts.query.order_by(Posts.date.desc()).all()
     return render_template("all_posts.html", params=params, posts=posts,active="posts")
 
-@app.route("/admin_login")
+@app.route("/admin_login",methods=['GET','POST'])
 def admin_login():
-    return render_template("admin_login.html")
+    
+    if ('user' in session and session['user']==params['admin_username']):
+         return render_template("dashboard.html",admin_name=session['user'])
+    
+    if (request.method=='POST'):
+        uname=request.form.get("username")
+        pswd=request.form.get("password")
+        if(uname==params['admin_username'] and pswd==params['admin_password']):
+            session['user']=uname
+            return render_template("dashboard.html",admin_name=session['user'])
+            #return dashboard.html
+            # return f"Successfully Authorized \nusername = {uname} and password is {pswd}"
+        else:
+            return "you are not authorized"
+    else:
+        return render_template("admin_login.html")
+    
+@app.route("/dashboard")
+def dashboard():
+    return redirect(url_for('dashboard_posts'))
+
+@app.route("/dashboard/posts")
+def dashboard_posts():
+    posts=Posts.query.order_by(Posts.date.desc()).all()
+    return render_template(
+        "dashboard_posts.html",
+        posts=posts,
+        admin_name=session['user']
+    )
+    
+@app.route('/dashboard/contacts')
+def dashboard_contacts():
+    contacts=Contact.query.all()
+    return render_template(
+        'dashboard_contacts.html',
+        contacts=contacts,
+        admin_name=session['user']
+    )
+    
+@app.route("/dashboard/settings")
+def dashboard_settings():
+    return render_template(
+        "dashboard_settings.html",
+        admin_name=session['user']
+    )
+    
+@app.route("/dashboard/posts/create_post",methods=['GET','POST'])
+def create_post():
+    
+    if 'user' not in session:
+        return redirect(url_for("admin_login"))
+    
+    if(request.method=="POST"):
+        
+        title=request.form.get('title')
+        content=request.form.get('content')
+        author=request.form.get('author')
+        slug=request.form.get('slug')
+        
+        entry=Posts(title=title,content=content,author=author,slug=slug,date=datetime.now())
+        
+        try:
+            db.session.add(entry)
+            db.session.commit()
+            return redirect(url_for("dashboard_posts"))
+        except Exception as e :
+            db.session.rollback()
+            return f"Error : {e}"
+        
+    return render_template(
+        "create_post.html",
+        admin_name=session['user']
+    )
+    
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return admin_login()
 
 @app.route("/posts/<string:post_slug>",methods=['GET'])
-
 def posts(post_slug):
     post=Posts.query.filter_by(slug=post_slug).first()
     return render_template('posts.html',params=params,post=post,active="posts")
